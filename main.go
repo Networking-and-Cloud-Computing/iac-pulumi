062468cf740c5240d339c991d953b617d33fad3a
joshichandana <joshi.chandana412@gmail.com>
2023-10-18 20:38:06 -0400

    Commit Updates

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 	"net"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,12 +18,15 @@ func main() {
 		vpcName := c.Require("vpcName")
 		destinationBlock := c.Require("destinationBlock")
 		publicSubnet := c.Require("publicSubnetName")
-		privateSubnet:= c.Require("privateSubnetName")
+		privateSubnet := c.Require("privateSubnetName")
 		internetGatewayName := c.Require("internetGatewayName")
 		publicRouteTableName := c.Require("publicRouteTableName")
 		privateRouteTableName := c.Require("privateRouteTableName")
 		publicRouteAssociationName := c.Require("publicRouteAssociationName")
 		privateRouteAssociationName := c.Require("privateRouteAssociationName")
+		instanceType := c.Require("instanceType")
+		publicSubnetID := c.Require("publicSubnetID")
+		amiID := c.Require("amiID")
 		availabilityZones, err := aws.GetAvailabilityZones(ctx, &aws.GetAvailabilityZonesArgs{
 			State: pulumi.StringRef("available"),
 		}, nil)
@@ -56,7 +59,69 @@ func main() {
 		for i, subnet := range subnets {
 			subnetStrings[i] = subnet.String()
 		}
+		//Create an ec2 Security Group
+		securityGroup, err := ec2.NewSecurityGroup(ctx, "webSecurityGroup", &ec2.SecurityGroupArgs{
+			Description: pulumi.String("Enable HTTP and SSH access"),
+			Egress:      ec2.SecurityGroupEgressArray{egressArgs("0.0.0.0/0", "all")},
+			Ingress: ec2.SecurityGroupIngressArray{
+				ingressArgs("0.0.0.0/0", "tcp", 22),
+				ingressArgs("0.0.0.0/0", "tcp", 80),
+				ingressArgs("0.0.0.0/0", "tcp", 443),
+				// Add additional port number that your application runs on.
+				ingressArgs("0.0.0.0/0", "tcp", 8080),
+			},
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("application_security_group"),
+			},
+		})
 
+		if err != nil {
+			return err
+		}
+		//// Look Up for the AMI
+		//ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
+		//	MostRecent: pulumi.BoolRef(true),
+		//	Filters: []ec2.GetAmiFilter{
+		//		{
+		//			Name: "name",
+		//			Values: []string{
+		//				"Cloud",
+		//			},
+		//		},
+		//		{
+		//			Name: "virtualization-type",
+		//			Values: []string{
+		//				"hvm",
+		//			},
+		//		},
+		//	},
+		//	Owners: []string{
+		//		"172266639576",
+		//	},
+		//}, nil)
+		//if err != nil {
+		//	return err
+		//}
+		// Create a New Ec2 Instance with the above Security Group
+		_, err = ec2.NewInstance(ctx, "Cloud", &ec2.InstanceArgs{
+			InstanceType:          pulumi.String(instanceType),
+			SubnetId:              pulumi.String(publicSubnetID),
+			VpcSecurityGroupIds:   pulumi.StringArray{securityGroup.ID()},
+			Ami:                   pulumi.String(amiID),
+			KeyName:               pulumi.String("Cloud"),
+			DisableApiTermination: pulumi.Bool(false),
+			RootBlockDevice: &ec2.InstanceRootBlockDeviceArgs{
+				VolumeSize: pulumi.Int(25),
+				VolumeType: pulumi.String("gp2"),
+			},
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("Cloud-EC2-Instance"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+		//ctx.Export("instanceID", instance.ID())
 		// Create 3 Public Subnets
 
 		publicSubnets := make([]*ec2.Subnet, 0, subnetCount)
@@ -67,7 +132,7 @@ func main() {
 				AvailabilityZone:    pulumi.String(availabilityZones.Names[i]),
 				MapPublicIpOnLaunch: pulumi.Bool(true),
 				Tags: pulumi.StringMap{
-					"Name": pulumi.String(publicSubnet+strconv.Itoa(i)),
+					"Name": pulumi.String(publicSubnet + strconv.Itoa(i)),
 				},
 			})
 			if err != nil {
@@ -84,7 +149,7 @@ func main() {
 				CidrBlock:        pulumi.String(subnetStrings[i+subnetCount]),
 				AvailabilityZone: pulumi.String(availabilityZones.Names[i]),
 				Tags: pulumi.StringMap{
-					"Name": pulumi.String(privateSubnet+strconv.Itoa(i)),
+					"Name": pulumi.String(privateSubnet + strconv.Itoa(i)),
 				},
 			})
 			if err != nil {
@@ -155,5 +220,23 @@ func main() {
 		}
 		ctx.Export("PublicRouteID", public_route.ID())
 		return err
+
 	})
+}
+
+func ingressArgs(cidr, protocol string, fromPort int) ec2.SecurityGroupIngressInput {
+	return ec2.SecurityGroupIngressArgs{
+		Protocol:   pulumi.String(protocol),
+		FromPort:   pulumi.Int(fromPort),
+		ToPort:     pulumi.Int(fromPort),
+		CidrBlocks: pulumi.StringArray{pulumi.String(cidr)},
+	}
+}
+func egressArgs(cidr, protocol string) ec2.SecurityGroupEgressInput {
+	return ec2.SecurityGroupEgressArgs{
+		Protocol:   pulumi.String(protocol),
+		FromPort:   pulumi.Int(0),
+		ToPort:     pulumi.Int(0),
+		CidrBlocks: pulumi.StringArray{pulumi.String(cidr)},
+	}
 }
